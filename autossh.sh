725e2bdcb2fc2fce8450bb6b9aeb76f67aacc07f
sunhanwu:huawei <1965190613@qq.com>
2022-12-02 00:25:11 +0800

    update autossh.sh

--- autossh.sh
+++ autossh.sh
@@ -15,7 +15,7 @@ echo "[ INFO ] 检测密钥对是否存在"
 if [ -f ~/.ssh/id_rsa ]; then
 	echo "[ INFO ] 密钥对已存在"
 else
-	echo "[ INFO ] 密钥对已存在"
+	echo "[ INFO ] 密钥对不存在，正在创建新的密钥对"
 	ssh-keygen -t rsa
 fi
 
@@ -24,25 +24,34 @@ ssh -o NumberOfPasswordPrompts=0 ${username}@${host} "pwd" &>/dev/null
 if [[ $? -ne 0 ]]; then
 	echo "[ WARN ] 未配置免密登录，正在配置"
 	ssh-copy-id ${username}@${host}
+else
+	echo "[ INFO ] 已经配置免密登录"
 fi
 
 echo "[ INFO ] 检测autossh是否安装"
 if command -v autossh >/dev/null 2>&1; then
 	echo "[ INFO ] autossh已安装"
 else
+	echo "[ INFO ] autossh未安装, 正在安装"
 	sudo apt-get install autossh < echo ${password}
 fi
 
-echo "[ INFO ] 进程是否存在"
+echo "[ INFO ] 检测进程是否存在"
 tmp=1000
 monitorPort=`expr $remotePort - $tmp`
 result=`ps -aux | grep -i "autossh -M $monitorPort -fCNR $remotePort:localhost:$port $username@$host" | grep -v "grep" | wc -l`
 if [ $result -ge 1 ]; then
-	echo "[ INFO ] 进程已存在，正在重启该任务"
-	pid=`ps -aux | grep -i "autossh -M $monitorPort -fCNR $remotePort:localhost:$port $username@$host" | grep -v "grep" | awk '{print $2}'`
-	kill -9 ${pid}
-	autossh -M $monitorPort -fCNR $remotePort:localhost:$port $username@$host
+	echo -e "[ INFO ] 进程已存在，是否重启该任务(Y/N): \c"
+	read choice
+	if [ "$choice" = "Y" ]; then
+		pid=`ps -aux | grep -i "autossh -M $monitorPort -fCNR $remotePort:localhost:$port $username@$host" | grep -v "grep" | awk '{print $2}'`
+		kill -9 ${pid}
+		autossh -M $monitorPort -fCNR $remotePort:localhost:$port $username@$host
+		echo "[ INFO ] 已重启该任务"
+	else
+		exit 8
+	fi
 else
-	echo "[ INFO ] 正在重启该任务"
+	echo "[ INFO ] 正在启动该任务"
 	autossh -M $monitorPort -fCNR $remotePort:localhost:$port $username@$host
 fi
